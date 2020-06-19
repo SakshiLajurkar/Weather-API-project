26ff46d8236de1f80c1f2d59716b9364fb8027a8
Sakshi Lajurkar <lajurkar.sakshi@gmail.com>
2020-06-19 13:24:24 +0530

    final

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ app.get("/", function(req, res) {
 });
 
 app.post("/", function(req, res) {
-  console.log(req.body.cityName);
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=187d0c51e421b551906b5c5b002dafce&units=metric"
+  const city = req.body.cityName;
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=187d0c51e421b551906b5c5b002dafce&units=metric";
 
   https.get(url, function(response) {
     console.log(response);
@@ -22,8 +22,10 @@ app.post("/", function(req, res) {
     response.on("data", function(data) {
       const weatherData = JSON.parse(data)
       const temp = weatherData.main.temp;
-
-      res.write("<h1>The temperature is " + temp+" degree Celcius.</h1>");
+      const weatherDescription = weatherData.weather[0].description
+      res.write(city + "   ")
+      res.write("Weather description: " + weatherDescription + "   ")
+      res.write("Temperature: " + temp+" °C");
       res.send()
     });
   });
@@ -31,5 +33,5 @@ app.post("/", function(req, res) {
 });
 
 app.listen(3000, function() {
-  console.log("welcome to port 3000");
+  console.log("Server running on port 3000");
 });
